79d3d96a121991704b8b66c9f000f09576443b5c
Bs1987 <boysax@gmail.com>
2021-05-04 11:28:31 +0200

    added comments

--- main.js
+++ main.js
@@ -163,108 +163,136 @@ const inventory = [
 ];
 
 function calculateTvToSellCount() {
+    // calculate how many tvs to sell by mapping originalstock and sold and substracting them
     let tvSoldArray = inventory.map((television) => {
         return television.originalStock - television.sold;
     })
-
+//start with empty counter "total"
     let total = 0;
 
     for (let i = 0; i < tvSoldArray.length; i++) {
         total += tvSoldArray[i];
-
+// through each loop, add the the amount of tvs left in stock to the total counter
     }
-
+//return total to call later on
     return total;
 }
 
 function displayTvToSellCount() {
+    //make a new variable "totalCount" and use the previous function to give it value
     const totalCount = calculateTvToSellCount();
+    //make a new variable "tvToSellElement to create an element on the page"
     const tvToSellElement = document.createElement("tvs-to-sell");
+    //add values to element
     tvToSellElement.textContent = `We need to sell: ${totalCount} tvs`;
     tvToSellElement.style.color = 'red';
+    //Stick it on the page
     document.body.appendChild(tvToSellElement);
 }
 
+//call function to stick element on page
 displayTvToSellCount();
 
+//make a variable for the amount of sold tvs to use later on
 const tvSold = inventory.map((television) => {
     return television.sold;
 });
+//make a variable for the amount of tvs in stock to use later on
 const tvInStock = inventory.map((television) => {
     return television.originalStock;
 })
+//loop through the whole inventory and calculate the amount of tvs left for each tv
 for (let i = 0; i < inventory.length; i++) {
     let tvsLeft = tvSold[i] - tvInStock[i];
+    //if the tvsold - tvinstock = 0, tvs are sold out
     if (tvsLeft === 0) {
     }
 }
 
+//map which tvs have the function "Ambilight"
 const tvAmbilight = inventory.map((television) => {
     return television.options.ambiLight;
 })
-
 for (let i = 0; i < inventory.length; i++) {
     if (tvAmbilight[i] === true) {
     }
 }
 
+//sort all the tvs by price by using the .sort() method.
 inventory.sort((televisionA, televisionB) => {
     return televisionA.price - televisionB.price;
 });
-
+//make an empty array to start
 let televisionTotalRevenuePerType = [];
+
+//make an empty variable tvTypeRevenue to fill later on
 let tvTypeRevenue = "";
 inventory.map((televisionType) => {
+    //in a loop, calculate the total revenue by calling the original stock and multiplying it by the tv type price
     for (let i = 0; i < inventory.length; i++) {
         tvTypeRevenue = televisionType.originalStock * televisionType.price;
     }
+    //for each loop, push the value in the empty array
     televisionTotalRevenuePerType.push(tvTypeRevenue);
 })
 
+//make a function to add all revenues together
 function totalRevenue() {
     let totalRevenue = 0;
-
+// for the length of the tvTypeRevenue array, add all values together to create the total revenue variable
     for (let i = 0; i < televisionTotalRevenuePerType.length; i++) {
         totalRevenue += televisionTotalRevenuePerType[i];
     }
     return totalRevenue;
 }
 
+//make a function to display the amount on the page
 function displayTotalExpectedRevenue() {
+    //make a variable and use to function totalRevenue() to fill it with a value
     const totalCount = totalRevenue();
+    //make a new variable to make a new element on the page
     const TotalExpectedRevenueElement = document.createElement("total-revenue");
     TotalExpectedRevenueElement.textContent = `We should make this ${totalCount} amount of money`;
     TotalExpectedRevenueElement.style.color = 'blue';
     document.body.appendChild(TotalExpectedRevenueElement);
 }
 
+//call function to stick it on the page
 displayTotalExpectedRevenue();
 
+//make empty variablese to fill later on
 let soldTvRevenuePerType = [];
 let soldTvRevenue = 0;
-
+//map the televisions and calculate the revenue from every sold tv
 inventory.map((televisionType) => {
     for (let i = 0; i < inventory.length; i++) {
+        //for each loop, calculate the amount of money made by selling tv types
         soldTvRevenue = televisionType.sold * televisionType.price;
     }
+    //for each loop, push the value in the empty array
     soldTvRevenuePerType.push(soldTvRevenue);
 })
 
+//make a function to add all previously calculated values
 function totalSoldTvRevenue() {
     let totalSoldTvRevenue = 0;
-
+//for each loop, add the value of soldTvRevenuePerType to the previous one
     for (let i = 0; i < soldTvRevenuePerType.length; i++) {
         totalSoldTvRevenue += soldTvRevenuePerType[i];
     }
     return totalSoldTvRevenue;
 }
 
+//make a function to display calculated values on the page
 function displayTSoldTvRevenue() {
+    //make variable to hold value from previous function
     const totalCount = totalSoldTvRevenue();
+    //make variable to create element to put on page
     const TotalTvSoldRevenueElement = document.createElement("tv-sold-revenue");
-    TotalTvSoldRevenueElement.textContent = `We should make this ${totalCount} amount of money`;
+    //fill element
+    TotalTvSoldRevenueElement.textContent = `We have made this ${totalCount} amount of money`;
     TotalTvSoldRevenueElement.style.color = 'green';
     document.body.appendChild(TotalTvSoldRevenueElement);
 }
-
+//call function to display on page
 displayTSoldTvRevenue();
